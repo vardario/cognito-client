09e73760c8f61a560bb44a473ee79381e7be1804
Sahin Vardar <sahin@vardar.io>
2023-11-15 20:00:15 +0100

    fix: release and build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['lib/**/*', 'node_modules']
+    exclude: ['lib/**/*', 'node_modules'],
+    hookTimeout: 60000
   }
 });
