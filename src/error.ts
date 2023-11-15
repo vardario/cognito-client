42b4017c6b2c7f4567d44e7f86d5f82c3d55fcbf
Sahin Vardar <sahin@vardar.io>
2023-11-15 10:03:06 +0100

    feat: unified cognito errors

--- src/error.ts
+++ src/error.ts
@@ -1,89 +1,149 @@
 /**
- * Possible Authentications errors
+ * Possible Cognito exceptions
  */
-export enum AuthError {
+export enum CognitoException {
   /**
-   * User already confirmed
+   * You do not have sufficient access to perform this action.
    */
-  UserConfirmedAlready = 'user_confirmed_already',
+  AccessDeniedException = "AccessDeniedException",
 
   /**
-   * User Profile does not exists.
+   * The request signature does not conform to AWS standards.
    */
-  UserDoesNotExist = 'user_does_not_exist',
+  IncompleteSignature = "IncompleteSignature",
 
   /**
-   * User Profile does not exists.
+   * The request processing has failed because of an unknown error, exception or failure.
    */
-  UserAlreadyExists = 'user_already_exists',
+  InternalFailure = "InternalFailure",
 
   /**
-   * Password was wrong.
+   * The action or operation requested is invalid. Verify that the action is typed correctly.
    */
-  EmailOrPasswordWrong = 'email_or_password_wrong',
+  InvalidAction = "InvalidAction",
 
   /**
-   * Rate limit exceeded.
+   * The X.509 certificate or AWS access key ID provided does not exist in our records.
    */
-  LimitExceededException = 'limit_exceeded_exception',
+  InvalidClientTokenId = "InvalidClientTokenId",
 
   /**
-   * User needs to be authenticated.
+   * You do not have permission to perform this action.
    */
-  UserNotAuthenticated = 'user_not_authenticated',
+  NotAuthorized = "NotAuthorized",
 
   /**
-   * The user tried to many times wiht the wrong password
+   * The AWS access key ID needs a subscription for the service.
    */
-  PasswordAttempsExceeded = 'password_attemps_exceeded',
+  OptInRequired = "OptInRequired",
 
   /**
-   * User E-Mail needs to be verified.
+   * The request reached the service more than 15 minutes after the date stamp on the request or more than 15 minutes after the request expiration date (such as for pre-signed URLs), or the date stamp on the request is more than 15 minutes in the future.
    */
-  UserEmailNotVerified = 'user_email_not_verified',
+  RequestExpired = "RequestExpired",
+
+  /**
+   * The request has failed due to a temporary failure of the server.
+   */
+  ServiceUnavailable = "ServiceUnavailable",
+
+  /**
+   * The request was denied due to request throttling.
+   */
+  ThrottlingException = "ThrottlingException",
+
+  /**
+   * The input fails to satisfy the constraints specified by an AWS service.
+   */
+  ValidationError = "ValidationError",
+
+  /**
+   * This exception is thrown when AWS WAF doesn't allow your request based on a web ACL that's associated with your user pool.
+   */
+  ForbiddenException = "ForbiddenException",
+
+  /**
+   * This exception is thrown when Amazon Cognito encounters an internal error.
+   */
+  InternalErrorException = "InternalErrorException",
+
+  /**
+   * This exception is thrown when Amazon Cognito encounters an invalid AWS Lambda response.
+   */
+  InvalidLambdaResponseException = "InvalidLambdaResponseException",
+
+  /**
+   * This exception is thrown when the Amazon Cognito service encounters an invalid parameter.
+   */
+  InvalidParameterException = "InvalidParameterException",
+
+  /**
+   * This exception is returned when the role provided for SMS configuration doesn't have permission to publish using Amazon SNS.
+   */
+  InvalidSmsRoleAccessPolicyException = "InvalidSmsRoleAccessPolicyException",
+
+  /**
+   * This exception is thrown when the trust relationship is not valid for the role provided for SMS configuration. This can happen if you don't trust cognito-idp.amazonaws.com or the external ID provided in the role does not match what is provided in the SMS configuration for the user pool.
+   */
+
+  InvalidSmsRoleTrustRelationshipException = "InvalidSmsRoleTrustRelationshipException",
+
+  /**
+   * This exception is thrown when the user pool configuration is not valid.
+   */
+  InvalidUserPoolConfigurationException = "InvalidUserPoolConfigurationException",
+
+  /**
+   * This exception is thrown when a user isn't authorized.
+   */
+  NotAuthorizedException = "NotAuthorizedException",
+
+  /**
+   * This exception is thrown when a password reset is required.
+   */
+  PasswordResetRequiredException = "PasswordResetRequiredException",
+
+  /**
+   * This exception is thrown when the Amazon Cognito service can't find the requested resource.
+   */
+  ResourceNotFoundException = "ResourceNotFoundException",
+
+  /**
+   * This exception is thrown when the user has made too many requests for a given operation.
+   */
+  TooManyRequestsException = "TooManyRequestsException",
+
+  /**
+   * This exception is thrown when Amazon Cognito encounters an unexpected exception with AWS Lambda.
+   */
+  UnexpectedLambdaException = "UnexpectedLambdaException",
+
+  /**
+   * This exception is thrown when the Amazon Cognito service encounters a user validation exception with the AWS Lambda service.
+   */
+  UserLambdaValidationException = "UserLambdaValidationException",
+
+  /**
+   * This exception is thrown when a user isn't confirmed successfully.
+   */
+  UserNotConfirmedException = "UserNotConfirmedException",
+
+  /**
+   * This exception is thrown when a user isn't found.
+   */
+  UserNotFoundException = "UserNotFoundException",
 
   /**
    * Unknown auth error happened.
    */
-  Unknown = 'unknown',
+  Unknown = "unknown",
 }
 
-export class AuthException extends Error {
-  public readonly authError: AuthError;
+export class CognitoError extends Error {
+  public readonly authError: CognitoException;
 
-  constructor(message: string, authError: AuthError) {
+  constructor(message: string, authError: CognitoException) {
     super(message);
     this.authError = authError;
   }
-}
-
-export enum ErrorCode {
-  UserNotFoundException = 'UserNotFoundException',
-  NotAuthorizedException = 'NotAuthorizedException',
-  LimitExceededException = 'LimitExceededException',
-}
-
-export interface CognitoAuthErrorResponse {
-  __type: ErrorCode;
-  message: string;
-}
-
-export function getAuthError(errorResponse: CognitoAuthErrorResponse) {
-  const mapping: Record<string, AuthError> = {
-    'UserNotFoundException:User cannot be confirmed. Current status is CONFIRMED': AuthError.UserConfirmedAlready,
-    'NotAuthorizedException:Incorrect username or password.': AuthError.EmailOrPasswordWrong,
-    'LimitExceededException:Attempt limit exceeded, please try after some time.': AuthError.LimitExceededException,
-    'UserNotFoundException:Username/client id combination not found.': AuthError.UserDoesNotExist,
-    'UserNotFoundException:User does not exist.': AuthError.UserDoesNotExist,
-    'NotAuthorizedException:Password attempts exceeded': AuthError.PasswordAttempsExceeded,
-    'UsernameExistsException:An account with the given email already exists.': AuthError.UserAlreadyExists,
-    'InvalidParameterException:Cannot reset password for the user as there is no registered/verified email or phone_number':
-      AuthError.UserEmailNotVerified,
-    'UserNotConfirmedException:User is not confirmed.': AuthError.UserEmailNotVerified,
-  };
-
-  const message = `${errorResponse.__type}:${errorResponse.message}`;
-  const authError = mapping[message] || AuthError.Unknown;
-
-  return new AuthException(message, authError);
 }
