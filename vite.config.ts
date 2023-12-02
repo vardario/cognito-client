a40c5af629c43c3ec3e45bc43fe5fb68f5cacff1
Sahin Vardar <sahin@vardar.io>
2023-12-02 17:42:25 +0100

    chore: fix integration testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['lib/**/*', 'node_modules', 'cognito-deployment/**/*', '**/*/integration.test.ts'],
+    exclude: ['lib/**/*', 'node_modules', 'cognito-deployment/**/*'],
     hookTimeout: 60000
   }
 });
