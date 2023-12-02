e9ab59e83ae99ab8514aa736f482e3aebb409bbb
Sahin Vardar <sahin@vardar.io>
2023-12-02 17:55:44 +0100

    chore: increased test timeout for integration test and fix env variable passing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: ['lib/**/*', 'node_modules', 'cognito-deployment/**/*'],
-    hookTimeout: 60000
+    hookTimeout: 60000,
+    testTimeout: 60000
   }
 });
